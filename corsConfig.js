29d4fffa796b0a583614daeee101e689410e14f6
wgibbs3237 <wgibbs3237@gmail.com>
2025-02-03 23:48:56 -0800

    chasebank

--- corsConfig.js
+++ corsConfig.js
@@ -12,7 +12,7 @@ const corsOptions = {
     origin: (origin, callback) => {
         const allowedOrigins = [
             'http://127.0.0.1:5501',  // Local development URL
-            'https://dkbank.vercel.app' // Your production frontend URL
+            'https://chasebank-mu.vercel.app' // Your production frontend URL
         ];// Add your frontend origin(s) here
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true); // Allow the request
